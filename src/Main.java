d70e1590cdc98794a63d63047872333278c7e202
Otito Amuga <urphasin@gmail.com>
2026-03-07 23:06:13 -0700

    gfgf

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,16 @@
 void main() {
+    //noinspection unused
     int[] rnos = new int[5];
 
+    //noinspection unused
     int[] rnos2 = {1, 2, 3, 4, 5};
 
-    pattern1(5);
+    pattern1(6);
     pattern2(5);
+
 }
 
-static void pattern1(int n) {
+static void pattern1(@SuppressWarnings("SameParameterValue") int n) {
     System.out.println("pattern 1");
     for(int row = 1; row <= n; ++row) {
         for (int col = 1; col <= row; ++col) {
@@ -18,6 +21,7 @@ static void pattern1(int n) {
     System.out.println();
 }
 
+@SuppressWarnings("SameParameterValue")
 static void pattern2(int n) {
     System.out.println("pattern 2");
     for(int row = 1; row <= n; ++row) {

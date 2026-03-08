b7209fe0b4abd768d714632bea8676a984439cf4
Otito Amuga <urphasin@gmail.com>
2026-03-07 23:03:08 -0700

    gfgf

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,27 @@ void main() {
     int[] rnos2 = {1, 2, 3, 4, 5};
 
     pattern1(5);
+    pattern2(5);
 }
 
 static void pattern1(int n) {
+    System.out.println("pattern 1");
     for(int row = 1; row <= n; ++row) {
         for (int col = 1; col <= row; ++col) {
             System.out.print("* ");
         }
         System.out.println();
     }
+    System.out.println();
 }
 
 static void pattern2(int n) {
-    f
+    System.out.println("pattern 2");
+    for(int row = 1; row <= n; ++row) {
+        for(int col = 1; col <= 5; ++col) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+    System.out.println();
 }
